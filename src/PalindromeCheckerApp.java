b8017ea9a9c096963f77b95769ded5745986cf95
bp8829 <reddybhanuprakash810@gmail.com>
2026-02-18 15:55:37 +0530

    UC4: Character array based palindrome check using two pointer

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker MANAGEMENT SYSTEM ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the text: ");
+        String text = scanner.nextLine();
+
+        // Convert string to character array
+        char[] characters = text.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Entered Text: " + text);
+        System.out.println("Is it Palindrome? : " + isPalindrome);
+
+        scanner.close();
     }
 
 }
